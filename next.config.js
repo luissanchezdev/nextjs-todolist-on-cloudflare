e6b0c563d686d1229c7880f4a7c5d07f565b08f8
Luis Sanchez <luis.sanchez.dev@gmail.com>
2024-12-06 07:33:21 -0500

    feature: cloudflare pages configurado

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
+
 const nextConfig = {}
+
+const cloudFlareSetupDevPlatform = async () => {
+  await setupDevPlatform();
+}
+
+if (process.env.NODE_ENV === 'development') {
+  cloudFlareSetupDevPlatform();
+}
 
 module.exports = nextConfig
